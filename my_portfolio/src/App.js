26b230502bb157ab3a9c9093780dd0daa11dbfb7
Ethan Moss <Ethanmoss2001@gmail.com>
2020-07-06 18:02:39 +0100

    Adding a github image with external hyperlink

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -4,10 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <span className="text_1">Hello, My name is Ethan Moss</span>
-      <span className="text_2">
-        I am hoping to become a front-end developer
-      </span>
+      <h1>
+        <span className="text_1">Hello, I'm Ethan Moss</span>
+        <span className="text_2">
+          I am hoping to become a front-end developer
+        </span>
+      </h1>
+      <a href="https://github.com/EthanM2001">
+        <img
+          className="github"
+          src="https://pbs.twimg.com/profile_images/1157035760085684224/iuxTnT5g_400x400.jpg"
+          alt="github"
+        />
+      </a>
     </div>
   );
 }
